82152554f7131dfa8d801db7d8e432e61abe5ae5
Beau Burke <beau@filterswept.com>
2013-12-01 16:50:48 -0800

    Use require.js to load scripts. Use cdnjs.com instead of local script copies. Move script tags for faster loading

--- src/js/app.js
+++ src/js/app.js
@@ -1,108 +1,118 @@
 "use strict";
 
-/*global window:false*/
+/*global require:false, window:false*/
+
+require.config({
+	paths: {
+		"jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min",
+		"bootstrap": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/js/bootstrap.min"
+	},
+	shim: {
+		"bootstrap": {
+			deps: ["jquery"]
+		}
+	}
+});
 
 /**
  * @file Game driver. Creates the game object and wires up the interface to respond to game state changes. It expects the following elements to be a part of the DOM.
  * @author Beau Burke <beau@filterswept.com>
  */
 
-(function (exports) {
-	var document = exports.document,
-		$ = exports.jQuery;
+require(
+	["jquery", "bootstrap", "classes"],
+	function ($, bootstrap, classes) {
+		$(document).ready(
+			function () {
+				var $surfaceTemplate = $("#surface_template"),
+					$meowFactory = $("#meow_sound_effects"),
+					game = new classes.HappycatGame($surfaceTemplate, $meowFactory),
+					labelMap = [
+						"bored",
+						"alert",
+						"playful",
+						"happy",
+						"excited",
+						"over-excited",
+						"annoyed",
+						"angry",
+						"ready to kill"
+					],
+					updateLabels = function () {
+						var $label = $("#score_descriptor"),
+							contents = labelMap[game.player.score] || labelMap[labelMap.length - 1];
 
-	$(document).on(
-		"ready",
-		function () {
-			var $surfaceTemplate = $("#surface_template"),
-				$meowFactory = $("#meow_sound_effects"),
-				game = new exports.HappycatGame($surfaceTemplate, $meowFactory),
-				labelMap = [
-					"bored",
-					"alert",
-					"playful",
-					"happy",
-					"excited",
-					"over-excited",
-					"annoyed",
-					"angry",
-					"ready to kill"
-				],
-				updateLabels = function () {
-					var $label = $("#score_descriptor"),
-						contents = labelMap[game.player.score] || labelMap[labelMap.length - 1];
+						$label.text(contents + ".");
 
-					$label.text(contents + ".");
-
-					if (game.player.score > 10) {
-						$("#instructions").text("oh god how did this get here I am not good with computer");
-					} else if (!game.player.isDefaultName()) {
-						$("#instructions").text(game.player.name + ", please pet happycat.");
-					} else {
-						$("#instructions").text("Please pet happycat.");
-					}
-				};
-
-			exports.game = game;
-			updateLabels();
-
-			game.onPlayerChange = function (player) {
-				updateLabels();
-
-				if (player.score >= 1) {
-					$("#pause_button").removeClass("disabled");
-					game.start();
-				}
-
-				if (player.score >= labelMap.length) {
-					game.addSurface();
-				}
-			};
-
-			game.introduce = function () {
-				var $introModal = $("#name_gather_modal").modal("show"),
-					$nameInput = $introModal.find("#name_input"),
-					$nameAcceptButton = $introModal.find("#name_accept_button"),
-					changeHandler = function (evt) {
-						if ($(this).val() !== "") {
-							$nameAcceptButton.removeClass("disabled");
+						if (game.player.score > 10) {
+							$("#instructions").text("oh god how did this get here I am not good with computer");
+						} else if (!game.player.isDefaultName()) {
+							$("#instructions").text(game.player.name + ", please pet happycat.");
 						} else {
-							$nameAcceptButton.addClass("disabled");
+							$("#instructions").text("Please pet happycat.");
 						}
 					};
 
-				$nameInput.on("keyup", changeHandler);
-				$nameInput.on("change", changeHandler);
+				updateLabels();
 
-				$nameAcceptButton.on(
+				game.onPlayerChange = function (player) {
+					updateLabels();
+
+					if (player.score >= 1) {
+						$("#pause_button").removeClass("disabled");
+						game.start();
+					}
+
+					if (player.score >= labelMap.length) {
+						game.addSurface();
+					}
+				};
+
+				game.introduce = function () {
+					var $introModal = $("#name_gather_modal").modal("show"),
+						$nameInput = $introModal.find("#name_input"),
+						$nameAcceptButton = $introModal.find("#name_accept_button"),
+						changeHandler = function (evt) {
+							if ($(this).val() !== "") {
+								$nameAcceptButton.removeClass("disabled");
+							} else {
+								$nameAcceptButton.addClass("disabled");
+							}
+						};
+
+					$nameInput.on("keyup", changeHandler);
+					$nameInput.on("change", changeHandler);
+
+					$nameAcceptButton.on(
+						"click",
+						function (evt) {
+							game.namePlayer($nameInput.val());
+							$introModal.modal("hide");
+						}
+					);
+				};
+
+				$("#pause_button").on(
 					"click",
 					function (evt) {
-						game.namePlayer($nameInput.val());
-						$introModal.modal("hide");
+						if ($(this).hasClass("active")) {
+							game.start();
+						} else {
+							game.pause();
+						}
 					}
 				);
-			};
 
-			$("#pause_button").on(
-				"click",
-				function (evt) {
-					if ($(this).hasClass("active")) {
-						game.start();
-					} else {
-						game.pause();
+				$("#reset_button").on(
+					"click",
+					function (evt) {
+						game.reset();
+						updateLabels();
+						$("#pause_button").addClass("disabled");
+						$("#pause_button").removeClass("active");
 					}
-				}
-			);
-
-			$("#reset_button").on(
-				"click",
-				function (evt) {
-					game.reset();
-					updateLabels();
-					$("#pause_button").addClass("disabled");
-					$("#pause_button").removeClass("active");
-				}
-			);
-		}
-	);
-}(window));
+				);
+			}
+		);
+	}
+);
